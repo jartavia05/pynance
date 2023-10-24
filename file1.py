49b98a3bfe96dba73be14dde4934c254af969473
Jose Artavia <jartavia05@gmail.com>
2023-10-24 15:38:23 -0600

    Initial commit

--- file1.py
+++ file1.py
@@ -1,7 +1,14 @@
 import pandas as pd 
 
-# Starting with the mining dogecoin setup 
-# r = return AND i = investment
+# # Starting with the mining dogecoin setup 
+''' Glossary 
+r = return 
+i = investment
+ROI = Return of Investment
+IRR = Internal Rate of Return
+
+'''
+
 r = 6000
 i = 3000
 print(f'{(r - i) / i * 100}%')
